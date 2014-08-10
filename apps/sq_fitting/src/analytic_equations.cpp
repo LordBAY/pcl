88af9bd2bb7ee40a639b645d28c29c0e29e1dec6
ana-GT <ahuaman3@gatech.edu>
2014-08-10 11:59:50 -0400

    Added working levmar with minimier

--- apps/sq_fitting/src/analytic_equations.cpp
+++ apps/sq_fitting/src/analytic_equations.cpp
@@ -843,3 +843,181 @@ double t461 = t178+t182;
 
 }
 
+
+/**
+ * @function levmar_fx
+ */
+void levmar_fx( double *p, double* x,
+		int m, int n, void *data ) {
+    
+    struct levmar_data* dptr;
+    dptr = (struct levmar_data*) data;
+    
+    double xi, yi, zi;
+    double t2, t3, t4, t5, t6;
+    double t7, t8, t9, t10;
+    double t11, t12, t13, t14, t15;
+    double t16, t17, t18, t19, t20;
+    double t21, t22;
+        
+    for( int i = 0; i < n; ++i ) {
+	
+	xi = dptr->x[i];
+	yi = dptr->y[i];
+	zi = dptr->z[i];
+	
+	t2 = cos(p[10]);
+	t3 = sin(p[8]);
+	t4 = cos(p[8]);
+	t5 = sin(p[9]);
+	t6 = sin(p[10]);
+	t7 = t3*t6;
+	t8 = t2*t4*t5;
+	t9 = t7+t8;
+	t10 = t2*t3;
+	t11 = t10-t4*t5*t6;
+	t12 = cos(p[9]);
+	t13 = p[5]*t9-p[6]*t11-t9*xi+t11*yi+p[7]*t4*t12-t4*t12*zi;
+	t14 = t4*t6;
+	t15 = t14-t2*t3*t5;
+	t16 = t2*t4;
+	t17 = t3*t5*t6;
+	t18 = t16+t17;
+	t19 = p[5]*t15-p[6]*t18-t15*xi+t18*yi-p[7]*t3*t12+t3*t12*zi;
+	t20 = p[7]*t5-t5*zi-p[5]*t2*t12-p[6]*t6*t12+t2*t12*xi+t6*t12*yi;
+	t21 = 1.0/p[4];
+	t22 = 1.0/p[3];
+	
+	x[i] = (pow(pow( pow(1.0/(p[0]*p[0])*(t20*t20),t21)+
+			 pow(1.0/(p[1]*p[1])*(t19*t19),t21),p[4]*t22 )+
+		    pow(1.0/(p[2]*p[2])*(t13*t13),t22), p[3] )
+		-1.0) * sqrt(p[0]*p[1]*p[2]);       		
+    } // end for
+    
+}
+
+
+/**
+ * @function levmar_jac
+ */
+void levmar_jac( double* p, double* jac,
+		 int m, int n, void* data ) {
+    
+    struct levmar_data* dptr;
+    dptr = (struct levmar_data*) data;
+    double xi, yi, zi;
+    
+    register double t2,t3,t4,t5;
+    register double t17, t18, t19, t20, t21, t22;
+    register double t6,t7,t8,t9;
+    register double t27, t10, t11, t12, t13;
+    register double t28, t29, t30, t31, t32, t33;
+    register double t14, t15, t16;
+    register double t23, t24, t25, t26;
+
+
+    register double t34, t35, t36, t37, t38, t39;
+    register double t40, t41, t42, t43, t44, t45, t46;
+    register double t50, t47, t49, t51, t52, t53, t54, t55;
+    register double t48, t56, t57, t58, t59;
+    register double t60, t61, t62, t63, t64, t65,t66;
+    register double t67, t68, t69, t70, t71, t72, t73, t74, t75;
+
+    for( int i = 0; i < n; ++i ) {
+
+	xi = dptr->x[i];
+	yi = dptr->y[i];
+	zi = dptr->z[i];
+
+	t2 = cos(p[10]);
+	t3 = sin(p[8]);
+	t4 = cos(p[8]);
+	t5 = sin(p[9]);
+	t6 = sin(p[10]);
+	t7 = t3*t6;
+	t8 = t2*t4*t5;
+	t9 = t7+t8;
+	t10 = t2*t3;
+	t25 = t4*t5*t6;
+	t11 = t10-t25;
+	t12 = cos(p[9]);
+	t24 = p[5]*t9;
+	t26 = p[6]*t11;
+	t27 = t9*xi;
+	t28 = t11*yi;
+	t29 = p[7]*t4*t12;
+	t30 = t4*t12*zi;
+	t13 = t24-t26-t27+t28+t29-t30;
+	t14 = t4*t6;
+	t35 = t2*t3*t5;
+	t15 = t14-t35;
+	t16 = t2*t4;
+	t17 = t3*t5*t6;
+	t18 = t16+t17;
+	t36 = p[5]*t15;
+	t37 = p[6]*t18;
+	t38 = t15*xi;
+	t39 = t18*yi;
+	t40 = p[7]*t3*t12;
+	t41 = t3*t12*zi;
+	t19 = t36-t37-t38+t39-t40+t41;
+	t46 = p[7]*t5;
+	t47 = t5*zi;
+	t48 = p[5]*t2*t12;
+	t49 = t2*t12*xi;
+	t50 = p[6]*t6*t12;
+	t51 = t6*t12*yi;
+	t20 = t46-t47-t48+t49-t50+t51;
+	t21 = 1.0/p[4];
+	t22 = 1.0/p[3];
+	t23 = 1.0/(p[2]*p[2]);
+	t31 = t13*t13;
+	t32 = t23*t31;
+	t33 = pow(t32,t22);
+	t34 = 1.0/(p[1]*p[1]);
+	t42 = t19*t19;
+	t43 = t34*t42;
+	t44 = pow(t43,t21);
+	t45 = 1.0/(p[0]*p[0]);
+	t52 = t20*t20;
+	t53 = t45*t52;
+	t54 = pow(t53,t21);
+	t55 = t44+t54;
+	t56 = p[4]*t22;
+	t57 = pow(t55,t56);
+	t58 = t33+t57;
+	t59 = p[0]*p[1]*p[2];
+	t60 = pow(t58,p[3]);
+	t61 = t60-1.0;
+	t62 = 1.0/sqrt(t59);
+	t63 = p[3]-1.0;
+	t64 = pow(t58,t63);
+	t65 = t21-1.0;
+	t66 = t56-1.0;
+	t67 = pow(t55,t66);
+	t68 = sqrt(t59);
+	t69 = 1.0/(p[3]*p[3]);
+	t70 = log(t55);
+	t71 = 1.0/(p[4]*p[4]);
+	t72 = pow(t43,t65);
+	t73 = pow(t53,t65);
+	t74 = t22-1.0;
+	t75 = pow(t32,t74);
+	jac[11*i+0] = p[1]*p[2]*t61*t62*(1.0/2.0)-1.0/(p[0]*p[0]*p[0])*t52*t64*t67*t68*t73*2.0;
+	jac[11*i+1] = p[0]*p[2]*t61*t62*(1.0/2.0)-1.0/(p[1]*p[1]*p[1])*t42*t64*t67*t68*t72*2.0;
+	jac[11*i+2] = p[0]*p[1]*t61*t62*(1.0/2.0)-1.0/(p[2]*p[2]*p[2])*t31*t64*t68*t75*2.0;
+	jac[11*i+3] = t68*(t60*log(t58)-p[3]*t64*(t33*t69*log(t32)+p[4]*t57*t69*t70));
+	jac[11*i+4] = p[3]*t64*t68*(t22*t57*t70-p[4]*t22*t67*(t44*t71*log(t43)+t54*t71*log(t53)));
+	jac[11*i+5] = p[3]*t64*t68*(p[4]*t22*t67*(t15*t19*t21*t34*t72*2.0-t2*t12*t20*t21*t45*t73*2.0)+t9*t13*t22*t23*t75*2.0);
+	jac[11*i+6] = -p[3]*t64*t68*(p[4]*t22*t67*(t18*t19*t21*t34*t72*2.0+t6*t12*t20*t21*t45*t73*2.0)+t11*t13*t22*t23*t75*2.0);
+	jac[11*i+7] = p[3]*t64*t68*(p[4]*t22*t67*(t5*t20*t21*t45*t73*2.0-t3*t12*t19*t21*t34*t72*2.0)+t4*t12*t13*t22*t23*t75*2.0);
+	jac[11*i+8] = p[3]*t64*t68*(t13*t19*t22*t23*t75*2.0-t13*t19*t22*t34*t67*t72*2.0);
+	jac[11*i+9] = p[3]*t64*t68*(p[4]*t22*t67*(t20*t21*t45*t73*(p[7]*t12-t12*zi+p[5]*t2*t5+p[6]*t5*t6-t2*t5*xi-t5*t6*yi)*2.0+t19*t21*t34*t72*(p[7]*t3*t5-t3*t5*zi-p[5]*t2*t3*t12-p[6]*t3*t6*t12+t2*t3*t12*xi+t3*t6*t12*yi)*2.0)-t13*t22*t23*t75*(p[7]*t4*t5-t4*t5*zi-p[5]*t2*t4*t12-p[6]*t4*t6*t12+t2*t4*t12*xi+t4*t6*t12*yi)*2.0);
+	jac[11*i+10] = p[3]*t64*t68*(p[4]*t22*t67*(t20*t21*t45*t73*(p[5]*t6*t12-p[6]*t2*t12-t6*t12*xi+t2*t12*yi)*2.0+t19*t21*t34*t72*(p[5]*t18+p[6]*t15-t18*xi-t15*yi)*2.0)+t13*t22*t23*t75*(p[5]*t11+p[6]*t9-t11*xi-t9*yi)*2.0);
+    } // end for
+    
+
+
+}
+
+
